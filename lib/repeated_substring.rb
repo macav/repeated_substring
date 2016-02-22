e15be02a1232a0fc4b7078600510ea9e2ffa5fae
Martin Cavoj <martin@panacom.sk>
2016-02-22 21:05:09 +0100

    Completed assignment

--- lib/repeated_substring.rb
+++ lib/repeated_substring.rb
@@ -1,7 +1,32 @@
 class RepeatedSubstring
   def find_repeated_substring(string)
-    # TODO: implement the function
-    1 + 2
+    size = string.length
+    substrings = Hash.new
+    size.times do |i|
+      size.times do |j|
+        substrings[string.slice(i, j)] = 0
+      end
+    end
+    # count occurences of each substring
+    substrings.each do |key, occurences|
+      key = key.gsub(/\n/, "")
+      if key.size > 1
+        substrings[key] = string.scan(/#{key}/).count
+      end
+    end
+    # pick only those with occurences >= 2, sort it by key length and value
+    substrings_sorted = substrings.select {|key, value| value >= 2}.sort_by {|k,v| [k.length, v]}.to_h
+    # out best guess
+    last_item = [substrings_sorted.keys.last, substrings_sorted.values.last]
+    if last_item[0] == nil
+      return "NONE"
+    end
+    # first occurence based on guess's key length and its count
+    top = substrings.select { |key, value| key.length == last_item[0].length && value >= last_item[1]}.first
+    if top[0] == nil
+      return "NONE"
+    end
+    return top[0]
   end
 
   def find_repeated_substring_file(file_path)
--- spec/repeated_substring_spec.rb
+++ spec/repeated_substring_spec.rb
@@ -2,9 +2,54 @@ require 'spec_helper'
 require 'repeated_substring'
 
 RSpec.describe 'repeated_substring' do
-  it 'is an example' do
+  it 'should print repeated substring for the most simple case' do
     rs = RepeatedSubstring.new
-    expect(rs.find_repeated_substring('banana')).to eq(3)
+    expect(rs.find_repeated_substring('oranange')).to eq('an')
+  end
+
+  it 'should print the first substring from the beginning of the string' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('banana')).to eq('an')
+  end
+
+  it 'should print NONE in case no repeated substrings are found' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('am so uniqe')).to eq('NONE')
+  end
+
+  it 'should not consider spaces as a correct substring' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('a   b')).to eq('NONE')
+  end
+
+  it 'should correctly find substring if there are spaces too' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('banana   orange')).to eq('an')
+  end
+
+  it 'should not count newlines as a valid substring' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring("a\n\n\n\na")).to eq('NONE')
+  end
+
+  it 'should not count national characters as regular' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('banäna')).to eq('NONE')
+  end
+
+  it 'should count substring containing spaces' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('ba na nana')).to eq('a n')
+  end
+
+  it 'should pick the longest substring if there are smaller substrings as well' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('banana oraorange')).to eq('ora')
+  end
+
+  it 'should not count overlapping substrings' do
+    rs = RepeatedSubstring.new
+    expect(rs.find_repeated_substring('anana')).to eq('an')
   end
 
   xit 'find the most repeated substring for every line of a file' do
